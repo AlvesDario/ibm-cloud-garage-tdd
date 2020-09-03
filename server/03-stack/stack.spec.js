c43c0115cd46b976bcd196be9c38cc98c61e1a38
Dario Alves <dario.alves@fatec.sp.gov.br>
2020-09-03 18:18:01 +0000

    stack size is one when pushed

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,9 +1,11 @@
 const stackFactory = () => {
   let empty = true;
+  let count = 0;
+
   return {
     isEmpty: () => empty,
-    size: () => 0,
-    push: () => { empty = false; }
+    size: () => count,
+    push: () => { empty = false; count++ }
   }
 };
 
@@ -17,6 +19,10 @@ describe('the stack canary spec', () => {
 });
 
 describe('a stack', () => {
+  beforeEach(() => {
+    stack = stackFactory();
+  })
+
   it('starts empty', () => {
     expect(stack.isEmpty()).toBe(true);
   });
@@ -27,7 +33,10 @@ describe('a stack', () => {
     stack.push();
     expect(stack.isEmpty()).toBe(false);
   });
-  it.todo('stack size is 1 when pushed');
+  it('stack size is 1 when pushed', () => {
+    stack.push();
+    expect(stack.size()).toBe(1);
+  });
   it.todo('stack is empty when pushed and popped');
   it.todo('stack size is 0 when pushed and popped');
   it.todo('throws overflow error when pushing to a stack at full capacity');
