937a4d0e87c5684a3919539d6f433bfbb9790602
Dario Alves <dario.alves@fatec.sp.gov.br>
2020-09-03 18:29:59 +0000

    stack throws underflow error

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -8,7 +8,10 @@ const stackFactory = () => {
       if (count === 2) throw new Error('capacity overflow error');
       count++;
     },
-    pop: () => count--
+    pop: () => {
+      if (count === 0) throw new Error('capacity underflow error');
+      count -= 1;
+    }
   }
 };
 
@@ -57,7 +60,11 @@ describe('a stack', () => {
       stack.push();
     }).toThrowError('capacity overflow error');
   });
-  it.todo('throw underflow error when popping an empty stack');
+  it('throw underflow error when popping an empty stack', () => {
+    expect(() => {
+      stack.pop();
+    }).toThrowError('capacity underflow error');
+  });
   it.todo('pops the same one item when pushed');
   it.todo('pops two items with the most recent first');
   it.todo('accepts only a positive capacity');
